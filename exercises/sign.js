7cf1888c3e4ecb97ae518a90e18fc38c30fa82ed
Christoph Abert <christoph.abert@telekom.de>
2020-01-09 16:42:16 +0100

    solved sign.js

--- exercises/sign.js
+++ exercises/sign.js
@@ -11,6 +11,14 @@
  */
 
 // Your code:
+function sign (num) {
+  if (num >0) {
+    return 1;
+  } else if (num==0) {
+    return 0;
+    }
+  return -1;
+}
 
 //* Begin of tests
 const assert = require('assert');
