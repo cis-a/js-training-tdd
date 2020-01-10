063bc3c8e97daed5dab06e873fd6d345b747cbaf
Christoph Abert <christoph.abert@telekom.de>
2020-01-10 11:31:41 +0100

    solved cut.js

--- exercises/cut.js
+++ exercises/cut.js
@@ -11,9 +11,30 @@
  */
 
 // Your code:
+function cutFirst (str) {
+  return str.substr(2, str.length-2);
+}
+
+function cutLast (str) {
+  return str.substr(0, str.length-2);
+}
+
+function cutFirstLast (str) {
+  return cutLast(cutFirst(str));
+}
 
 //* Begin of tests
 const assert = require('assert');
 
-assert.fail('You must write your own tests');
+//assert.fail('You must write your own tests');
+assert.strictEqual(typeof cutFirst, 'function');
+assert.strictEqual(typeof cutLast, 'function');
+assert.strictEqual(typeof cutFirstLast, 'function');
+assert.strictEqual(cutFirst.length, 1);
+assert.strictEqual(cutLast.length, 1);
+assert.strictEqual(cutFirstLast.length, 1);
+assert.deepStrictEqual(cutFirst('abc'), 'c');
+assert.deepStrictEqual(cutLast('abc'), 'a');
+assert.deepStrictEqual(cutFirstLast('abcde'), 'c');
+assert.deepStrictEqual(cutFirstLast('abc'), '');
 // End of tests */
