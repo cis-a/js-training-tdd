41fc3539ed040f4ee501113ca5956ddb9c94c2cd
Christoph Abert <christoph.abert@telekom.de>
2020-01-10 12:04:14 +0100

    solved jaden-case.js

--- exercises/jaden-case.js
+++ exercises/jaden-case.js
@@ -11,9 +11,19 @@
  */
 
 // Your code:
+function jadenCase (str) {
+  var arr = str.split(" ");
+  let build = '';
+  for (let word of arr) {
+    build+=word.substr(0,1).toUpperCase()+word.substr(1, word.length-1).toLowerCase()+' ';
+    }
+return build.substr(0, build.length-1);
+}
 
 //* Begin of tests
 const assert = require('assert');
 
-assert.fail('You must write your own tests');
+assert.strictEqual(typeof jadenCase, 'function');
+assert.strictEqual(jadenCase.length, 1);
+assert.deepStrictEqual(jadenCase('how are you ?'), 'How Are You ?');
 // End of tests */
