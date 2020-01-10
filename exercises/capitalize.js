62065f77dd9d06195f3828ca3cb2e9ce8b0b8de0
Christoph Abert <christoph.abert@telekom.de>
2020-01-10 11:44:37 +0100

    solved capitalize.js

--- exercises/capitalize.js
+++ exercises/capitalize.js
@@ -8,6 +8,9 @@
  */
 
 // Your code:
+function capitalize (str) {
+  return str.substr(0,1).toUpperCase()+str.substr(1, str.length-1).toLowerCase();
+}
 
 //* Begin of tests
 const assert = require('assert');
